4ddbc5e3f782c314c74bf75770adb743b487d0aa
도수호 <tngh147258@kumoh.ac.kr>
2023-08-02 23:40:24 +0900

    회원 도메인 실행과 테스트

---
+++ spring-core/src/main/java/com/example/springcore/member/Member.java
@@ -0,0 +1,14 @@
+package com.example.springcore.member;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Member {
+    private Long id;
+    private String name;
+    private Grade grade;
+
+
+}
