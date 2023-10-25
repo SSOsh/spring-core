3972f108fdacc6863093a576e2dceb5d61760f92
SSOsh <tngh147258@kumoh.ac.kr>
2023-10-25 10:05:58 +0900

    기본편 마무리

--- src/main/java/com/example/springcore/web/LogDemoController.java
+++ src/main/java/com/example/springcore/web/LogDemoController.java
@@ -3,6 +3,7 @@ package com.example.springcore.web;
 import com.example.springcore.common.MyLogger;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,15 +13,20 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
+//    private final ObjectProvider<MyLogger> myLoggerProvider;
     private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody
-    public String logDemo(HttpServletRequest request) {
+    public String logDemo(HttpServletRequest request) throws InterruptedException {
         String requestUrl = request.getRequestURL().toString();
+//        MyLogger myLogger = myLoggerProvider.getObject();
+
+        System.out.println("myLogger = " + myLogger.getClass());
         myLogger.setRequestURL(requestUrl);
 
         myLogger.log("controller test");
+        Thread.sleep(1000);
         logDemoService.logic("testId");
         return "OK";
     }
