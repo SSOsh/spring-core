e5f0e048c85d0159bacde30b0bdd940f26ce012f
SSOsh <tngh147258@kumoh.ac.kr>
2023-11-29 22:03:34 +0900

    로그 추적기 v1

---
+++ springavanced/src/main/java/com/example/springavanced/SpringavancedApplication.java
@@ -0,0 +1,13 @@
+package com.example.springavanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringavancedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringavancedApplication.class, args);
+	}
+
+}
