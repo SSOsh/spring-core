22b70d30328e418f5207c5ee93471410228e27a6
SSOsh <tngh147258@kumoh.ac.kr>
2024-02-09 12:06:34 +0900

    ThreadLocal 동기화 - 적용

--- springavanced/src/main/java/com/example/springavanced/LogTraceConfig.java
+++ springavanced/src/main/java/com/example/springavanced/LogTraceConfig.java
@@ -2,6 +2,7 @@ package com.example.springavanced;
 
 import com.example.springavanced.trace.logtrace.FieldLogTrace;
 import com.example.springavanced.trace.logtrace.LogTrace;
+import com.example.springavanced.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 public class LogTraceConfig {
     @Bean
     public LogTrace logTrace() {
-        return new FieldLogTrace();
+//        return new FieldLogTrace();
+        return new ThreadLocalLogTrace();
     }
 }
