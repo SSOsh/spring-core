0b3d5fcba2108f88b948979e57f1b460aab2eace
SSOsh <tngh147258@kumoh.ac.kr>
2024-04-06 10:40:17 +0900

    구체 클래스 기반 프록시 - 적용

--- proxy/src/test/java/hello/proxy/pureproxy/concreteproxy/ConcreteProxyTest.java
+++ proxy/src/test/java/hello/proxy/pureproxy/concreteproxy/ConcreteProxyTest.java
@@ -2,6 +2,7 @@ package hello.proxy.pureproxy.concreteproxy;
 
 import hello.proxy.pureproxy.concreteproxy.code.ConcreateLogic;
 import hello.proxy.pureproxy.concreteproxy.code.ConcreteClient;
+import hello.proxy.pureproxy.concreteproxy.code.TimeProxy;
 import org.junit.jupiter.api.Test;
 
 public class ConcreteProxyTest {
@@ -10,6 +11,14 @@ public class ConcreteProxyTest {
         ConcreateLogic concreateLogic = new ConcreateLogic();
         ConcreteClient client = new ConcreteClient(concreateLogic);
 
+        client.execute();
+    }
+
+    @Test
+    void addProxy() {
+        ConcreateLogic concreateLogic = new ConcreateLogic();
+        TimeProxy timeProxy = new TimeProxy(concreateLogic);
+        ConcreteClient client = new ConcreteClient(timeProxy);
         client.execute();
     }
 }
