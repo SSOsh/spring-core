100bf94b5f9bb551b35048bce2c50b8af6150f5d
SSOsh <tngh147258@kumoh.ac.kr>
2024-04-09 23:46:45 +0900

    CGLIB - 예제 코드

---
+++ proxy/src/test/java/hello/proxy/cglib/CglibTest.java
@@ -0,0 +1,25 @@
+package hello.proxy.cglib;
+
+import hello.proxy.cglib.code.TimeMethodInterceptor;
+import hello.proxy.common.service.ConcreteService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.cglib.proxy.Enhancer;
+
+@Slf4j
+public class CglibTest {
+    @Test
+    void cglib() {
+        ConcreteService target = new ConcreteService();
+
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(ConcreteService.class);
+        enhancer.setCallback(new TimeMethodInterceptor(target));
+        //concreteservice를 상속받아 프록시를 만들기때문에 강제 캐스팅 가능
+        ConcreteService proxy = (ConcreteService) enhancer.create();
+        log.info("targetClass={}", target.getClass());
+        log.info("proxyClass={}", proxy.getClass());
+
+        proxy.call();
+    }
+}
