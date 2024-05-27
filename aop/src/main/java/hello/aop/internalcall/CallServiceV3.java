bc66153d63a140721e8d1d1cc952fa263d40a580
SSOsh <tngh147258@kumoh.ac.kr>
2024-05-27 21:47:12 +0900

    프록시와 내부 호출 - 대안3 구조 변경

---
+++ aop/src/main/java/hello/aop/internalcall/CallServiceV3.java
@@ -0,0 +1,23 @@
+package hello.aop.internalcall;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Component;
+
+/**
+ * 구조를 번경(분리)
+ * spring에서 가장 추천하는 방법
+ */
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class CallServiceV3 {
+
+    private final InternalService internalService;
+
+    public void external() {
+        log.info("call external");
+        internalService.internal();
+    }
+}
